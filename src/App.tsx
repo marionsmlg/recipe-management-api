60ec6d9f2f718315703869421e60e2be249d5126
Marion Schimmerling <marion.schimmerling2@hotmail.fr>
2024-06-18 10:24:13 +0200

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Layout from './components/Layout.tsx'
 
-
-function App() {
+export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+    <>
+      <Layout>Hello world</Layout>
+    </>
   )
 }
 
-export default App
+
---
+++ src/components/Layout.tsx
@@ -0,0 +1,167 @@
+import { useState } from 'react'
+import { Dialog, DialogPanel, Transition, TransitionChild } from '@headlessui/react'
+import {
+    Bars3Icon,
+    HomeIcon,
+    XMarkIcon,
+    BeakerIcon,
+    SparklesIcon,
+    ExclamationCircleIcon
+} from '@heroicons/react/24/outline'
+
+const navigation = [
+    { name: 'Dashboard', href: '/', icon: HomeIcon, current: true },
+    { name: 'Ingredients', href: '/', icon: BeakerIcon, current: false },
+    { name: 'Benefits', href: '/', icon: SparklesIcon, current: false },
+    { name: 'Allergens', href: '/', icon: ExclamationCircleIcon, current: false },
+]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function Navbar({ children }) {
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+
+    return (
+        <>
+            <div>
+                <Transition show={sidebarOpen}>
+                    <Dialog className="relative z-50 lg:hidden" onClose={setSidebarOpen}>
+                        <TransitionChild
+                            enter="transition-opacity ease-linear duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="transition-opacity ease-linear duration-300"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <div className="fixed inset-0 bg-gray-900/80" />
+                        </TransitionChild>
+
+                        <div className="fixed inset-0 flex">
+                            <TransitionChild
+                                enter="transition ease-in-out duration-300 transform"
+                                enterFrom="-translate-x-full"
+                                enterTo="translate-x-0"
+                                leave="transition ease-in-out duration-300 transform"
+                                leaveFrom="translate-x-0"
+                                leaveTo="-translate-x-full"
+                            >
+                                <DialogPanel className="relative mr-16 flex w-full max-w-xs flex-1">
+                                    <TransitionChild
+                                        enter="ease-in-out duration-300"
+                                        enterFrom="opacity-0"
+                                        enterTo="opacity-100"
+                                        leave="ease-in-out duration-300"
+                                        leaveFrom="opacity-100"
+                                        leaveTo="opacity-0"
+                                    >
+                                        <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
+                                            <button type="button" className="-m-2.5 p-2.5" onClick={() => setSidebarOpen(false)}>
+                                                <span className="sr-only">Close sidebar</span>
+                                                <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                                            </button>
+                                        </div>
+                                    </TransitionChild>
+
+                                    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-2 ring-1 ring-white/10">
+                                        <div className="flex h-16 shrink-0 items-center">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="stroke-2 text-white lucide lucide-droplets"><path d="M7 16.3c2.2 0 4-1.83 4-4.05 0-1.16-.57-2.26-1.71-3.19S7.29 6.75 7 5.3c-.29 1.45-1.14 2.84-2.29 3.76S3 11.1 3 12.25c0 2.22 1.8 4.05 4 4.05z" /><path d="M12.56 6.6A10.97 10.97 0 0 0 14 3.02c.5 2.5 2 4.9 4 6.5s3 3.5 3 5.5a6.98 6.98 0 0 1-11.91 4.97" /></svg>
+                                        </div>
+                                        <nav className="flex flex-1 flex-col">
+                                            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                                                <li>
+                                                    <ul role="list" className="-mx-2 space-y-1">
+                                                        {navigation.map((item) => (
+                                                            <li key={item.name}>
+                                                                <a
+                                                                    href={item.href}
+                                                                    className={classNames(
+                                                                        item.current
+                                                                            ? 'bg-gray-800 text-white'
+                                                                            : 'text-gray-400 hover:bg-gray-800 hover:text-white',
+                                                                        'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6',
+                                                                    )}
+                                                                >
+                                                                    <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
+                                                                    {item.name}
+                                                                </a>
+                                                            </li>
+                                                        ))}
+                                                    </ul>
+                                                </li>
+
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                </DialogPanel>
+                            </TransitionChild>
+                        </div>
+                    </Dialog>
+                </Transition>
+
+                {/* Static sidebar for desktop */}
+                <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+                    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
+                        <div className="flex h-16 shrink-0 items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="stroke-2 text-white lucide lucide-droplets"><path d="M7 16.3c2.2 0 4-1.83 4-4.05 0-1.16-.57-2.26-1.71-3.19S7.29 6.75 7 5.3c-.29 1.45-1.14 2.84-2.29 3.76S3 11.1 3 12.25c0 2.22 1.8 4.05 4 4.05z" /><path d="M12.56 6.6A10.97 10.97 0 0 0 14 3.02c.5 2.5 2 4.9 4 6.5s3 3.5 3 5.5a6.98 6.98 0 0 1-11.91 4.97" /></svg>
+
+                        </div>
+                        <nav className="flex flex-1 flex-col">
+                            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                                <li>
+                                    <ul role="list" className="-mx-2 space-y-1">
+                                        {navigation.map((item) => (
+                                            <li key={item.name}>
+                                                <a
+                                                    href={item.href}
+                                                    className={classNames(
+                                                        item.current
+                                                            ? 'bg-gray-800 text-white'
+                                                            : 'text-gray-400 hover:bg-gray-800 hover:text-white',
+                                                        'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6',
+                                                    )}
+                                                >
+                                                    <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
+                                                    {item.name}
+                                                </a>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </li>
+
+                                <li className="-mx-6 mt-auto">
+                                    <div
+                                        className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-gray-800"
+                                    >
+                                        <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
+                                            M
+                                        </span>
+                                        <span aria-hidden="true">Marion Schimmerling</span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+
+                <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-900 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+                    <button type="button" className="-m-2.5 p-2.5 text-gray-400 lg:hidden" onClick={() => setSidebarOpen(true)}>
+                        <span className="sr-only">Open sidebar</span>
+                        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                    <div className="flex-1 text-sm font-semibold leading-6 text-white">Dashboard</div>
+                    <span className="sr-only">Your profile</span>
+                    <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white">
+                        M
+                    </span>
+                </div>
+
+                <main className="py-10 lg:pl-72">
+                    <div className="px-4 sm:px-6 lg:px-8">{children}</div>
+                </main>
+            </div>
+        </>
+    )
+}
